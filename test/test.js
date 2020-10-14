10759e10c7a4594d1de8fc6fa2158481cbe6fcb0
tienquan1999 <zed.quan99@gmail.com>
2020-10-14 21:56:17 +0700

    init

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const ATM = require("../index");
+const assert = require("assert");
+
+describe("ATM", () => {
+    it('Something', (done) => {
+        done(new Error("fail"))
+    })
+    it('Something', () => {
+        
+    })
+})
+
+// let result = ATM({
+//     id: 1,
+//     amount: 1500000
+// })
+
+// console.log(result);
